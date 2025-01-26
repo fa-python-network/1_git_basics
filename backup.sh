c0a49cf1651c29e369db85af77d99f1091d74e83
csmcrlw <lidaaaserova@gmail.com>
2025-01-26 23:17:05 +0500

    new lines

--- backup.sh
+++ backup.sh
@@ -2,3 +2,6 @@ echo "#!/bin/bash" > ~/git/test/backup.sh
 echo "echo 'This is a backup script.'" >> ~/git/test/backup.sh
 chmod +x ~/git/test/backup.sh
 echo "# This is a comment" >> backup.sh
+echo "echo 'Another change'" >> backup.sh
+git add backup.sh
+git commit -m "Изменение 1"
